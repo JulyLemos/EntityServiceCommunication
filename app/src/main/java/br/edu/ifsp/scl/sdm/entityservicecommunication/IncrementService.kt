73eaa13e10d965894bf966c145d42d08840e175a
July Lemos da Silva <julylemos1@hotmail.com>
2026-03-12 20:06:40 -0300

    refactor: lógica de incrementação em uma nova thread com HandlerThread

--- app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunication/IncrementService.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunication/IncrementService.kt
@@ -2,13 +2,36 @@ package br.edu.ifsp.scl.sdm.entityservicecommunication
 
 import android.app.Service
 import android.content.Intent
+import android.os.Handler
+import android.os.HandlerThread
 import android.os.IBinder
+import android.os.Looper
+import android.os.Message
 
 class IncrementService : Service() {
 
+    private inner class IncrementHandler(looper: Looper): Handler(looper){
+        override fun handleMessage(msg: Message) {
+            super.handleMessage(msg)
+            msg.data.getInt("VALUE").also {
+                InterEntityCommunication.valueLiveData.postValue(it + 1) //livedata que continua a ser observado no main activity
+            } //o trabalho do serviço vai ser executado numa thread separada
+            stopSelf()
+        }
+    }
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        intent?.getIntExtra("VALUE", -1)?.also {
-            InterEntityCommunication.valueLiveData.postValue(it + 1)
+        intent?.getIntExtra("VALUE", -1)?.also { value ->
+            HandlerThread("IncrementThread").apply { //vai pegar a mensagem e o valor que veio dentro dessa mensagem
+                start()
+                IncrementHandler(looper).apply {
+                    //não é adequado ficar criando objetos do tipo message
+                    obtainMessage().apply {
+                        data.putInt("VALUE", value)
+                        sendMessage(this)
+                    }
+                }
+            }
         }
         return START_NOT_STICKY
     }
